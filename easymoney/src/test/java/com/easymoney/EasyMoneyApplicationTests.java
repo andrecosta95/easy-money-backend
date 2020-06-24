2f7bc51e84920eb70e8149c6d798ae0cf0650526
André Costa <luz_ia_costa@hotmail.com>
2020-06-23 23:54:54 -0300

    Correcting class names and adding dependencies

--- easymoney/src/test/java/com/easymoney/EasyMoneyApplicationTests.java
+++ easymoney/src/test/java/com/easymoney/EasyMoneyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EasymoneyApplicationTests {
+class EasyMoneyApplicationTests {
 
 	@Test
 	void contextLoads() {
